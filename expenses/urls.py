bb772628cf4b2a1d787400392e37e888195be9e4
Akhadafi <khadafi.dev@gmail.com>
2022-07-03 13:41:13 +0700

    Setting Up Urls_Views_Templates

--- expenses/urls.py
+++ expenses/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("expense.urls")),
+    path("authentication/", include("authentication.urls")),
+    path("admin/", admin.site.urls),
 ]
